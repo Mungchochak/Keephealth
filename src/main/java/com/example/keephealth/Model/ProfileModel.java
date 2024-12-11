44e8572c397d9d85aa584fc664e0f0a64ce9f060
GuoZhanbo789 <guozhanbo500@gmail.com>
2024-12-11 17:37:11 +0800

    new

---
+++ src/main/java/com/example/keephealth/Model/ProfileModel.java
@@ -0,0 +1,82 @@
+package com.example.keephealth.Model;
+
+public class ProfileModel {
+    String name;
+    String gender;
+    String email;
+    String phone;
+    String weight;
+    String height;
+    int age;
+    int currentid;
+
+    public int getCurrentid() {
+        return currentid;
+    }
+
+    public void setCurrentid(int currentid) {
+        this.currentid = currentid;
+    }
+
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return currentid+"/"+name+"/"+gender+"/"+email+"/"+phone+"/"+weight+"/"+height+"/"+age;
+    }
+}
